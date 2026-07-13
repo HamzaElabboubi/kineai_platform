a997c074a42b705c327f643b9b3b8a56982c905d
HamzaElabboubi <elabboubihamza8@gmail.com>
2026-07-13 15:18:05 +0100

    feat: add HTTPS with Let's Encrypt certificate

--- backend/src/main/java/com/project/kineai/config/SecurityConfig.java
+++ backend/src/main/java/com/project/kineai/config/SecurityConfig.java
@@ -102,8 +102,8 @@ public class SecurityConfig {
                 "http://localhost:4200",
                 "http://51.170.140.90:4200",
                 "https://kineai.app",
-                "http://kineai.online",        // ← ajouter
-                "http://www.kineai.online"
+                "https://kineai.online",        // ← ajouter
+                "https://www.kineai.online"
         ));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://kineai.online:8080'
+  apiUrl: 'https://kineai.online:8080'
 };
